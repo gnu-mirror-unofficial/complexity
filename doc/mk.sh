099362d1d2e422d0ba240c1362d1d3f7a6b12580
Bruce Korb <bruce.korb@gmail.com>
2020-10-11 15:29:48 -0700

    Resurrect bootstrapping
    a lot of stuff moved on, leaving this code atrophied
    
    Files changed:
    * bootstrap: update from libtool
    * bootstrap.conf: likewise
    * bootstrap.stc: likewise
    * doc/Makefile.am: dependencies work a bit different
    * doc/complexity-tpl.texi: don't try to symlink into source tree
    * doc/mk.sh: make it more debuggable

--- doc/mk.sh
+++ doc/mk.sh
@@ -1,10 +1,21 @@
 #! /bin/sh
 
+if test "X$VERBOSE" = Xtrue
+then
+    exec 2> /tmp/MK-SH.txt
+    set -x
+    PS4='+DOC-${FUNCNAME:-=}=$LINENO> '
+else
+    VERBOSE=false
+fi
+
 ag=`command -v autogen`
 test -x "$ag" || {
     touch $1
     exit 0
 }
+top_builddir=`cd $top_builddir && pwd`
+top_srcdir=`cd $top_srcdir && pwd`
 
 test -x ${top_builddir}/src/${exe} || {
     cd ${top_builddir}/src || exit 1
@@ -14,6 +25,17 @@ test -x ${top_builddir}/src/${exe} || {
 
 set -e
 touch ${1}-TMP
-$ag -T${texi_tpl} -b ${exe} ${optdefs}
-$ag ${TXI_DEP} -Tagtexi-cmd -DLEVEL=chapter ${optdefs}
+
+if $VERBOSE
+then
+    export AUTOGEN_TRACE=every
+    export AUTOGEN_TRACE_OUT=/tmp/AG-man.txt
+    $ag -T${texi_tpl} -b ${exe} ${optdefs}
+    AUTOGEN_TRACE_OUT=/tmp/AG-info.txt
+    $ag ${TXI_DEP} -Tagtexi-cmd -DLEVEL=chapter ${optdefs}
+else
+    $ag -T${texi_tpl} -b ${exe} ${optdefs}
+    $ag ${TXI_DEP} -Tagtexi-cmd -DLEVEL=chapter ${optdefs}
+fi
+
 mv ${1}-TMP ${1}
